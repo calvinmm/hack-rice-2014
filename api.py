98628abce33b0d2485d918e5a7791a593d2a5db3
Calvin MacKenzie <calvinm.mackenzie@gmail.com>
2014-01-25 17:20:58 -0600

    course table init

--- api.py
+++ api.py
@@ -7,15 +7,27 @@ def index():
     # temporary?
     return "HACK RICE"
 
+def get_conn():
+    # bad form
+    conn = sqlite3.connect('test.db')
+    return conn.cursor()
+
+def query(query):
+    return get_conn().execute(query)
+
 @app.route('/cats')
 def cats():
-    # bad form but whatever
-    conn = sqlite3.connect('test.db')
-    c = conn.cursor()
-    rows = c.execute('select * from cats')
+    rows = query('select * from cats')
 
     # jsonify the results
     return jsonify(cats=list(rows))
+
+@app.route('/courses')
+def courses():
+    rows = query('select * from courses')
+
+    # json json
+    return jsonify(courses=list(rows))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
---
+++ setup_table.py
@@ -0,0 +1,60 @@
+import sqlite3
+
+def main():
+    db_name = 'test.db'
+
+    def create_table():
+        conn = sqlite3.connect(db_name)
+        c = conn.cursor()
+        c.execute('''create table courses (dept text, number integer, name text, dist integer)''')
+        conn.commit()
+        conn.close()
+
+    def clean_table():
+        conn = sqlite3.connect(db_name)
+        c = conn.cursor()
+        c.execute("drop table courses")
+        conn.commit()
+        conn.close()
+
+    def check_table():
+        conn = sqlite3.connect(db_name)
+        c = conn.cursor()
+        try:
+            results = list(c.execute("select * from courses"))
+        except:
+            results = []
+        conn.commit()
+        conn.close()
+        
+        if len(results) != 0:
+            print "WARNING: Table contains data"
+            i = raw_input("Continue (y/n)? >>> ")
+            if i != "y":
+                import sys
+                sys.exit(0)
+            clean_table()
+        else:
+            clean_table()
+            create_table()
+
+    check_table()
+
+    course_data = "course_data.txt"
+    with open(course_data, "r") as f:
+        for line in f:
+            
+            tup = tuple(line.split("~"))
+            assert len(tup) == 4
+
+            def insert_row(tup):
+                conn = sqlite3.connect(db_name)
+                c = conn.cursor()
+                c.execute("insert into courses values ('%s', '%s', '%s', '%s')" % tup)
+                conn.commit()
+                conn.close()
+
+            insert_row(tup)
+
+if __name__ == '__main__':
+    main()
