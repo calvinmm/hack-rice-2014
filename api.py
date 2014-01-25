4f4122a74409f89fc4aad3b9d9128d5bf00523c5
Calvin MacKenzie <calvinm.mackenzie@gmail.com>
2014-01-25 17:01:20 -0600

    json json

--- api.py
+++ api.py
@@ -1,17 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
 import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # temporary?
     return "HACK RICE"
 
 @app.route('/cats')
 def cats():
+    # bad form but whatever
     conn = sqlite3.connect('test.db')
     c = conn.cursor()
     rows = c.execute('select * from cats')
-    return str(list(rows))
+
+    # jsonify the results
+    return jsonify(cats=list(rows))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
