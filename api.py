c7698e831be4700b88be8801de466c78959e5433
Calvin MacKenzie <calvinm.mackenzie@gmail.com>
2014-01-25 16:52:57 -0600

    db

--- api.py
+++ api.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "HACK RICE"
 
+@app.route('/cats')
+def cats():
+    conn = sqlite3.connect('test.db')
+    c = conn.cursor()
+    rows = c.execute('select * from cats')
+    return str(list(rows))
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
