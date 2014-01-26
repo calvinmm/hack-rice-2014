2be6c7fa2932b45f310971c53ccbb5812b097f2e
TRP3 <theodore.popp3@gmail.com>
2014-01-25 19:43:55 -0600

    Love Tres

--- js/choosing.js
+++ js/choosing.js
@@ -5,4 +5,8 @@ angular.module("hr",[])
  'Search' : "search.html",
  'intro' : "intro.html",
  };
+ $scope.courses = {};
+ $scope.load = function(){
+     $scope.courses = $http.jsonp("courses?callback=JSON_CALLBACK");
+ };
  });
