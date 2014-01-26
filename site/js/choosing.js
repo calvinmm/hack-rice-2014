f7dd00e47e815dd283999e40fb52e26df5f319f0
TRP3 <theodore.popp3@gmail.com>
2014-01-26 00:50:31 -0600

    I'm scared

--- site/js/choosing.js
+++ site/js/choosing.js
@@ -6,18 +6,24 @@ angular.module("hr",[])
  'intro' : "intro.html",
  };
     $scope.courses = [];
+    $scope.division = [];
+    $scope.distribution = [];
  $scope.load = function(){
      $http({method: "JSONP", url: "http://ec2-54-201-138-192.us-west-2.compute.amazonaws.com:8080/courses?callback=JSON_CALLBACK", cache : $templateCache}).
       success(function(data, status) {
         $scope.coursessss = data["courses"];
         angular.forEach($scope.coursessss, function(follower, index){
                 $scope.courses.push(follower);
-            console.log(follower);
-            });
-      }).
+            if($scope.division.indexOf(follower["dept"]) === -1){
+                $scope.division.push(follower["dept"]);
+            };
+            if($scope.distribution.indexOf(follower["dist"]) === -1){
+                $scope.distribution.push(follower["dist"]);
+            };
+      })}).
       error(function(data, status) {
         $scope.data = data || "Request failed";
         $scope.status = status;
-    });
-  };
- });
+    })
+ };
+});
