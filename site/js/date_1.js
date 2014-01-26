f46711b24ba351bb4becc81388c465df016ae716
Calvin MacKenzie <calvinm.mackenzie@gmail.com>
2014-01-26 09:21:08 -0600

    working

--- site/js/date_1.js
+++ site/js/date_1.js
@@ -1,3 +1,35 @@
 (function() {
     // date_1.js
 }());
+
+function determine_top(time) {
+    var times = ['8a', '9a', '10a', '11a', '12p',
+                 '1p', '2p', '3p', '4p', '5p', '6p', '7p'];
+
+    var bottom = 26;
+    return (bottom + times.indexOf(time) * bottom).toString() + "px";
+}
+
+function determine_left(day) {
+    var days = ['M', 'T', 'W', 'TH', 'F'];
+    var left = [21, 148, 276, 401, 528];
+
+    return left[days.indexOf(day)].toString() + "px";
+}
+
+function add_class_calendar(day, time, name, id) {
+    console.log(day, time);
+    var top = determine_top(time);
+    var left = determine_left(day);
+
+    var d = $("<div>", {
+        class: "class-element"
+    }).css("top", top)
+      .css("left", left)
+      .text(name)
+      .attr("id", id);
+      
+      // .css("background-color", "blue");
+
+    $("#add-here").append(d);
+}
